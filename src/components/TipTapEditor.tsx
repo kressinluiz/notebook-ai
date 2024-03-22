986ab4c4f68da1dee4ee03f3b0c443bbc2f8002b
kressin <luiz.kressin@gmail.com>
2024-03-21 21:54:59 -0300

    add text autocomplete openai; add kbd ui;

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -8,12 +8,17 @@ import { useDebounce } from '@/lib/useDebounce'
 import { NoteType } from '@/lib/db/schema'
 import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
+import Text from "@tiptap/extension-text"
+import {useCompletion} from 'ai/react'
 
 type Props = {note: NoteType}
 
 const TipTapEditor = ({note}: Props) => {
 	const [editorState, setEditorState] = React.useState(note.editorState || `<h1>${note.name}</h1>`)
 	const [isSaving, setIsSaving] = React.useState(false)
+	const {complete, completion} = useCompletion({
+		api: '/api/completion'
+	})
 	const saveNote = useMutation({
 		mutationFn: async () => {
 			const response = axios.post('/api/saveNote', {
@@ -23,14 +28,35 @@ const TipTapEditor = ({note}: Props) => {
 			return (await response).data
 		} 
 	})
+	const customText = Text.extend({
+		addKeyboardShortcuts() {
+			return {
+				"Shift-A": () => {
+					const prompt = this.editor.getText().split(' ').slice(-30).join(' ')
+					console.log("prompt: " + prompt)
+					complete(prompt)
+					return true
+				},
+			};
+		},
+	});
+
 	const editor = useEditor({
 		autofocus: true,
-		extensions: [StarterKit],
+		extensions: [StarterKit, customText],
 		content: editorState,
 		onUpdate: ({editor}) => {
 			setEditorState(editor.getHTML())
 		},
 	});
+
+	const lastCompletion = React.useRef('')
+	React.useEffect(() => {
+		if (!completion || !editor) return
+		const diff = completion.slice(lastCompletion.current.length)
+		lastCompletion.current = completion
+		editor?.commands.insertContent(diff)
+	}, [completion, editor])
 
 	const debouncedEditorState = useDebounce(editorState, 300)
 
@@ -56,9 +82,17 @@ const TipTapEditor = ({note}: Props) => {
 					{saveNote.isPending ? "Saving..." : "Saved"}
 				</Button>
 			</div>
-			<div className='prose'>
+			<div className='prose prose-sm w-full mt-4'>
 				<EditorContent editor={editor}/>
 			</div>
+			<div className="h-4"></div>
+			<span className='text-sm'>
+				Tip: Press {" "}
+				<kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg">
+					Shift + A
+				</kbd>{" "}
+				for AI autocomplete
+			</span>
 		</>
 	) 
 }
